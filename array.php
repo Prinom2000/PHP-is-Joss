873b3ff2a66d7dfe6553bda812e0b54f6849bbd7
Prinom Mojumder <130492223+Prinom2000@users.noreply.github.com>
2024-03-11 02:08:30 +0600

    Add files via upload

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php
+$arr= array("pm", "am", "pq", "xx");
+echo $arr[0];
+
+// here, element of array has ID
+
+$a=array( "a"=>22, "b"=>" prinom ","c"=>44,"d"=>99);
+echo $a["a"];
+echo $a["b"];
+echo $a["c"];
+echo $a["d"];
+
+
+?>
--- prinom.php
+++ prinom.php
@@ -3,4 +3,23 @@ echo "Prinom Mojumder...! <br>" ;
 print("prinom......!");
 echo "<P>Prinom Mojumder...!</p>"; // it takes a padding..........!
 
+
+//Concatenation:
+
+$t1="prinom";
+$t2="mojumder";
+echo $t1, " ", $t2, "<br>" ;
+echo $t1.$t2, "<br>";
+echo $t1, "<br>";
+echo $t2, "<br>";
+
+//If the string has a set of double quotation marks
+$st="\"CSE\"";
+print("$st");
+
+$x1=5;
+$x2=10;
+$x3= $x1.$x2;
+echo $x3/10;
+
 ?>
