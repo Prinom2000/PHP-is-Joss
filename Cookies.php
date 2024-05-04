784db0025479c1eea31fcdc3ffe5b2b70be7a2d6
Prinom Mojumder <130492223+Prinom2000@users.noreply.github.com>
2024-05-04 20:27:57 +0600

    Add files via upload

---
+++ Cookies.php
@@ -0,0 +1,10 @@
+<?php
+echo"Hello Prinom........!<br>";
+
+// syntex of cookie..
+
+setcookie("id","2021-2-60-098", time() + 86400,"/");   // / for using cookies in hole wabsite.
+
+echo "The cookies is set.....!";
+
+?>
