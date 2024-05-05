3a7807f45aaab91ff87bc7b53ec1ed4c123e0428
Prinom Mojumder <130492223+Prinom2000@users.noreply.github.com>
2024-05-05 14:46:04 +0600

    Add files via upload

---
+++ Session/Session.php
@@ -0,0 +1,10 @@
+<?php
+// session: used to mannege info to diff page....!
+
+session_start();
+$_SESSION   ["name"] = "Prinom";
+$_SESSION   ["pass"] = "000";
+
+echo"session is Created.......!";
+
+?>
